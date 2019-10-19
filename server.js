916c10f2194e8bb335768052ee5b73ca2757faa6
Unni Ramachandran <unnipbvr@gmail.com>
2019-10-19 19:27:50 -0400

    Version 1

---
+++ server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Handlebars
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+
+app.set("view engine", "handlebars");
+
+// Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app); 
+
+console.clear();
+var syncOptions = {};
+syncOptions.force = process.env.SYNC_MODEL === "true" ? true : false;
+
+// Starting the server, syncing our models ------------------------------------/
+  app.listen(PORT, function () {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+
+module.exports = app;
