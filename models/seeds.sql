92942a4fbb4fc3b7ddb32c45e4b31faab4e10049
Unni Ramachandran <unnipbvr@gmail.com>
2019-10-20 12:01:36 -0400

    Finalizing project page

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO `portfolio`.`Projects`
+INSERT INTO `Projects`
 (`projectName`,`projectUrl`,`projectRepo`,`projectImageUrl`,`projectDescription`,`createdAt`,`updatedAt`)
 VALUES
 ("ROBINHOOD","https://limitless-bayou-30453.herokuapp.com/",
@@ -7,16 +7,36 @@ VALUES
 "Bootcamp Team Project - Marketplace application for streamlining food donations",
 now(),now()
 ),
-("TRAVEL PLANNER","https://github.com/unnikrishnan-r/Bamazon/blob/master/bamazon_demo.gif",
-"https://github.com/unnikrishnan-r/Bamazon",
+("TRAVEL PLANNER","https://unnikrishnan-r.github.io/Travel-Planner/",
+"https://github.com/unnikrishnan-r/Travel-Planner",
 "https://i.imgur.com/JhahZGg.png",
 "Bootcamp Team Project - Travel Planner application",
 now(),now()
 ),
-("BAMAZON","https://unnikrishnan-r.github.io/Travel-Planner/",
-"https://github.com/unnikrishnan-r/Travel-Planner",
+("BAMAZON","https://github.com/unnikrishnan-r/Bamazon/blob/master/bamazon_demo.gif",
+"https://github.com/unnikrishnan-r/Bamazon",
 "https://i.imgur.com/Sc9OtUA.png",
 "CLI version of an eCommerce website. Capable of handling multiple stakeholders",
+now(),now()
+),
+(
+"NBA Trivia", "https://unnikrishnan-r.github.io/NBA-2019-Trivia/",
+"https://github.com/unnikrishnan-r/NBA-2019-Trivia",
+"https://i.imgur.com/bpP0xHN.png",
+"Javascript based NBA Quiz",
+now(),now()
+),
+(
+"LIRI - Personal Assistant","https://github.com/unnikrishnan-r/LIRI-Node-App/blob/master/demo.gif",
+"https://github.com/unnikrishnan-r/LIRI-Node-App",
+"https://i.imgur.com/qR2DRfA.jpg",
+"Node based CLI Personal Assistant",
+now(),now()
+),
+("Rock Paper Scissors!","https://unnikrishnan-r.github.io/RPS-Online-Game/game.html",
+"https://github.com/unnikrishnan-r/RPS-Online-Game",
+"https://i.imgur.com/eHGy0sF.jpg",
+"jQuery based Online RPS game, using Firebase",
 now(),now()
 )
 ;
