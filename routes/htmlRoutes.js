dad1bc5c34c2285da273f2c2630036a960e212c0
Unni Ramachandran <unnipbvr@gmail.com>
2019-10-20 09:15:30 -0400

    Setting up projects page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,5 +2,7 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.render("index");
       });
-    
+      app.get("/projects", function (req, res) {
+        res.render("projects");
+      });   
 };
